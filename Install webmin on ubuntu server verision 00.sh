2088979cce0eabd8a6ce72c86f2bda4d1f2a4eb1
chingmichaellee <61435725+chingmichaellee@users.noreply.github.com>
2020-02-25 14:53:22 +0800

    Update Install webmin on ubuntu server verision 00.sh

--- Install webmin on ubuntu server verision 00.sh
+++ Install webmin on ubuntu server verision 00.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-
+# Setup script for installing Webmin to all Ubuntu based system.
+# By Charlie Barkin. (General Public License version 2.0) version 0.1
 ## This script is use for installing Webmin on Ubuntu Server
-## Version 0.01
+## Version 0.1
 
 # Install Curl
+
 sudo apt install curl -y
 
 # Add Repository to the system
@@ -13,6 +15,7 @@ echo "deb http://webmin.mirror.somersettechsolutions.co.uk/repository sarge cont
 curl http://www.webmin.com/jcameron-key.asc | sudo apt-key add -
 
 # Update Repository and install webmin
+
 sudo apt-get update
 sudo apt-get install webmin -y
 
